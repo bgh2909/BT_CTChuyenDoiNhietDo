475446e240ee3433ac086102b3f7a52c8e98cd62
bgh2909 <bgh2909@gmail.com>
2024-02-23 20:26:55 +0700

    Add project files.

---
+++ bt_ctchuyendoinhietdo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt_ctchuyendoinhietdo
+{
+    internal class Program
+    {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (9.0 / 5) * celsius + 32;
+            return fahrenheit;
+        }
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (5.0 / 9) * (fahrenheit - 32);
+            return celsius;
+        }
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Fahrenheit to Celsius");
+                Console.WriteLine("2. Celsius to Fahrenheit");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter fahrenheit: ");
+                        fahrenheit = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Fahrenheit to Celsius: "+ FahrenheitToCelsius(fahrenheit));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter celsius: ");
+                        celsius = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Celsius to Fahrenheit: " + CelsiusToFahrenheit(celsius));
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.WriteLine();
+            }
+            while (choice != 0);
+
+            Console.ReadKey();
+        }
+    }
+}
